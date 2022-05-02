8a8d47455bc78a4e6ac00c387e23a9501fa02d71
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-05-02 17:28:15 +0200

    esercizio concluso, working for bonus

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const app = new Vue ({
+    el: '#app',
+
+    data: {
+        myMails: []
+    },
+
+    methods: {
+
+    },
+    created() {
+        
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
+    for (i=0; i<=10; i++){
+    this.myMails.push(response.data.response);
+    }
+    }).catch(function (error) {
+    console.log(error);
+    })
+    }
+    })
