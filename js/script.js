5c7287f7b842f5dc96c868955222e4c70e80e45c
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-05-02 18:41:26 +0200

    corretto bonus

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ const app = new Vue ({
         mail: null,
         myMails: [],
         loading: true,
+        index: 0,
     },
 
     methods: {
 
     },
     created() {
-        for (i = 0; i <= 10; i++) {
+        for (i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
 
                 this.mail = response.data.response;
@@ -19,7 +20,7 @@ const app = new Vue ({
 
             }).catch(function (error) {
                 console.log(error);
-            }).finally(() => this.loading = false)
+            }).finally(() => this.index++)
         }
     }
 })
