57aeb209353e421766742968f7a0768b0f8f82f8
giuseppecantafio <giuseppecantafio@hotmail.it>
2022-05-02 21:10:30 +0200

    coretto refuso riga 15

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const app = new Vue ({
 
     },
     created() {
-        for (i = 0; i < 10; i++) {
+        for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
 
                 this.mail = response.data.response;
